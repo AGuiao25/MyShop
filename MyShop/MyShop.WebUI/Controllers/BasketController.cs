8de832cda417067dc64f80d7d6262030d1fb97c6
AGuiao25 <66593846+AGuiao25@users.noreply.github.com>
2020-09-23 03:53:40 +0800

    Added order service

--- MyShop/MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,12 @@ namespace MyShop.WebUI.Controllers
     public class BasketController : Controller
     {
         IBasketService basketService;
+        IOrderService orderService;
 
-        public BasketController(IBasketService BasketService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService)
         {
             this.basketService = BasketService;
+            this.orderService = OrderService;
         }
 
         // GET: Basket
@@ -46,5 +49,31 @@ namespace MyShop.WebUI.Controllers
 
             return PartialView(basketSummary);
         }
-    }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Checkout(Order order)
+        {
+            var basketItems = basketService.GetBasketItems(this.HttpContext);
+            order.OrderStatus = "Order Created";
+
+            //process payment
+
+            order.OrderStatus = "Payment Processed";
+            orderService.CreateOrder(order, basketItems);
+            basketService.ClearBasket(this.HttpContext);
+
+            return RedirectToAction("Thanyou", new { OrderId = order.Id });
+
+        }
+
+        public ActionResult ThankYou (string OrderId)
+        {
+            ViewBag.OrderId = OrderId;
+            return View();
+        }
+     }
 }
